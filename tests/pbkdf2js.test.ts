e6a543ebd99da56efc5a6d860c0fd5e6e11b7fd2
xkid1 <elmer.alluad@gmail.com>
2022-08-30 20:05:01 +0800

    test folder

---
+++ tests/pbkdf2js.test.ts
@@ -0,0 +1,16 @@
+const pbkdf2js = require('../src');
+
+describe('PBKDF2JS', () => {
+    it('should be defined', () => {
+        expect(pbkdf2js).toBeDefined();
+    }),
+        it('decrypt should be a function', () => {
+            // expect(typeof pbkdf2js.decrypt).toBe('function');
+        }),
+        it('encrypt should be a function', () => {
+            // expect(typeof pbkdf2js.encrypt).toBe('function');
+        });
+    // it('should return a PBKDF2JS instance', () => {
+    //     expect(pbkdf2js() instanceof pbkdf2js).toBe(true);
+    // });
+});
