4dff5ae9ef6c22640e238b84f6cf615e77860391
xkid1 <elmer.alluad@gmail.com>
2022-09-04 16:22:07 +0800

    test the class instance

--- tests/pbkdf2js.test.ts
+++ tests/pbkdf2js.test.ts
@@ -1,16 +1,33 @@
 import { describe, expect, test, it } from '@jest/globals';
+import { webcrypto } from 'crypto';
+const JsPbkdf2 = require('../src/js-pbkdf2');
+const jsPbkdf2 = new JsPbkdf2(
+    typeof window !== 'undefined' ? crypto : webcrypto
+);
 
-describe('PBKDF2JS', () => {
-    // it('should be defined', () => {
-    //     expect(pbkdf2js).toBeDefined();
-    // }),
-    //     it('decrypt should be a function', () => {
-    //         // expect(typeof pbkdf2js.decrypt).toBe('function');
-    //     }),
-    //     it('encrypt should be a function', () => {
-    //         // expect(typeof pbkdf2js.encrypt).toBe('function');
-    //     });
-    // it('should return a PBKDF2JS instance', () => {
-    //     expect(pbkdf2js() instanceof pbkdf2js).toBe(true);
-    // });
+describe('JsPbkdf2', () => {
+    it('should be defined', () => {
+        expect(JsPbkdf2).toBeDefined();
+    }),
+        it('should be a function to encrypt', () => {
+            expect(typeof jsPbkdf2.encryptData).toBe('function');
+        }),
+        it('should be a function to decrypt', () => {
+            expect(typeof jsPbkdf2.decryptData).toBe('function');
+        }),
+        it('should return a JsPbkdf2 instance', () => {
+            expect(jsPbkdf2 instanceof JsPbkdf2).toBe(true);
+        }),
+        test('Ecrypt should return a string', async () => {
+            const password = 'password';
+            const data = 'test sample';
+            const result = await jsPbkdf2.encryptData(password, data);
+            expect(typeof result).toBe('string');
+        }),
+        test('Decrypt should return a string', async () => {
+            const password = 'password';
+            const data = 'clurRNgu0sJAB5lI60NAtw==';
+            const result = await jsPbkdf2.decryptData(password, data);
+            expect(typeof result).toBe('string');
+        });
 });
