a3af9b4ef96d6bde114737f33e83060b0bd67dcc
xkid1 <elmer.alluad@gmail.com>
2023-02-26 21:11:42 +0800

    Change to module

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,26 @@
-export interface PBKDF2Interface {
-    encryptData(password: string, data: string): Promise<string>;
-    decryptData(password: string, data: string): Promise<string>;
+/**
+ * PBKDF2 is a key derivation function that can be used to derive keys from a password.
+ * It can be useda password based key derivation function (PBKDF2) or a password based key stretching function (PBKDF2-HMAC).
+ * and it can be used from the frontend or the backend. to encrypt and decrypt data.
+ * @param isCrypto  - reuired to determine if it is a browser or nodejs
+ * @function encryptData - encrypt data with a password
+ * @function decryptData - decrypt data with a password
+ */
+declare module 'js-pbkdf2' {
+    export class JsPbkdf2 {
+        constructor(isCrypto: Crypto);
+        /**
+         * encrypt data, return base64 string
+         * @param password - password must be required for encrypt and decrypt
+         * @param data - The data must be Json string for encrypt
+         */
+        encryptData(password: string, data: string): Promise<string>;
+
+        /**
+         * decrypt data, return string
+         * @param password - password must be required for decrypt
+         * @param data - data for decrypt
+         */
+        decryptData(password: string, data: string): Promise<string>;
+    }
 }
