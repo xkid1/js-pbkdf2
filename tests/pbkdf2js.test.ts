ce251e35698cea26d927e8e13f8d6bdec7f06502
xkid1 <elmer.alluad@gmail.com>
2023-02-26 21:12:43 +0800

    change name

--- tests/pbkdf2js.test.ts
+++ tests/pbkdf2js.test.ts
@@ -1,33 +1,31 @@
 import { describe, expect, test, it } from '@jest/globals';
 import { webcrypto } from 'crypto';
-const JsPbkdf2 = require('../src/js-pbkdf2');
-const jsPbkdf2 = new JsPbkdf2(
-    typeof window !== 'undefined' ? crypto : webcrypto
-);
+const JsPbkdf2 = require('../src/JsPbkdf2');
+const Pbkdf2 = new JsPbkdf2(typeof window !== 'undefined' ? crypto : webcrypto);
 
 describe('JsPbkdf2', () => {
     it('should be defined', () => {
-        expect(JsPbkdf2).toBeDefined();
+        expect(Pbkdf2).toBeDefined();
     }),
         it('should be a function to encrypt', () => {
-            expect(typeof jsPbkdf2.encryptData).toBe('function');
+            expect(typeof Pbkdf2.encryptData).toBe('function');
         }),
         it('should be a function to decrypt', () => {
-            expect(typeof jsPbkdf2.decryptData).toBe('function');
+            expect(typeof Pbkdf2.decryptData).toBe('function');
         }),
         it('should return a JsPbkdf2 instance', () => {
-            expect(jsPbkdf2 instanceof JsPbkdf2).toBe(true);
+            expect(Pbkdf2 instanceof JsPbkdf2).toBe(true);
         }),
         test('Ecrypt should return a string', async () => {
             const password = 'password';
             const data = 'test sample';
-            const result = await jsPbkdf2.encryptData(password, data);
+            const result = await Pbkdf2.encryptData(password, data);
             expect(typeof result).toBe('string');
         }),
         test('Decrypt should return a string', async () => {
             const password = 'password';
             const data = 'clurRNgu0sJAB5lI60NAtw==';
-            const result = await jsPbkdf2.decryptData(password, data);
+            const result = await Pbkdf2.decryptData(password, data);
             expect(typeof result).toBe('string');
         });
 });
