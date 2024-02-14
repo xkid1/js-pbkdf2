fd4bacadb78f927428b715f35aa2ad7e698464bb
xkid1 <elmer.alluad@gmail.com>
2024-02-14 11:44:05 +0800

    frontend class

--- src/js-pbkdf2-fe.ts
+++ src/js-pbkdf2-fe.ts
@@ -1,68 +1,79 @@
-// import { Base, Common, Helper } from "~/lib";
+import { Base, Helper, Common } from '../lib';
 
-// /**
-//  * @description Fronted pbkd2 encryption and decryption
-//  * @class JSPBKDF2FE
-//  */
-// class JSPBKDF2FE extends Base {
-//   constructor() {
-//     if (typeof window === 'undefined') {
-//       throw new Error('window is not defined');
-//     }
-//     super(window.crypto, new Uint8Array(16).buffer, new Uint8Array(16).buffer);
-//   }
+/**
+ * @description Fronted pbkd2 encryption and decryption
+ * @class JSPBKDF2FE
+ */
+class JSPBKDF2FE extends Base {
+  private instanceHelper: Helper;
+  private instanceCommon: Common;
+  constructor() {
+    super();
+    this.instanceHelper = new Helper(
+      window.crypto,
+      new Uint8Array(16).buffer,
+      new Uint8Array(16).buffer
+    );
+    this.instanceCommon = new Common();
+  }
 
-//   /**
-//    * encrypt data, return base64 string
-//    * @param password - password must be required for encrypt and decrypt
-//    * @param data - The data must be string, json string
-//    */
-//   encrypt(password: string, data: string): Promise<string> {
-//     if (password === '' && data === '') {
-//       throw new Error('password and data is required');
-//     }
-//     if (typeof password !== 'string' && typeof data !== 'string') {
-//       throw new Error('password and data must be string');
-//     }
-//     return new Promise(async (resolve, reject) => {
-//       try {
-//         const passkey = await this.passkey(password);
-//         const aesKey = await this.aesKey(passkey);
-//         const dataBuffer = this.stringToBuffer(data);
-//         const resultBuffer = await this.aesEncrypt(aesKey, dataBuffer);
-//         resolve(this.bufferToBase64(resultBuffer));
-//       } catch (error) {
-//         reject(error);
-//       }
-//     });
-//   }
+  /**
+   * encrypt data, return base64 string
+   * @param password - password must be required for encrypt and decrypt
+   * @param data - The data must be string, json string
+   */
+  encrypt(password: string, data: string): Promise<string> {
+    if (password === '' && data === '') {
+      throw new Error('password and data is required');
+    }
+    if (typeof password !== 'string' && typeof data !== 'string') {
+      throw new Error('password and data must be string');
+    }
+    return new Promise(async (resolve, reject) => {
+      try {
+        const passkey = await this.instanceHelper.passkey(password);
+        const aesKey = await this.instanceHelper.aesKey(passkey);
+        const dataBuffer = this.instanceCommon.stringToBuffer(data);
+        const resultBuffer = await this.instanceHelper.aesEncrypt(
+          aesKey,
+          dataBuffer
+        );
+        resolve(this.instanceCommon.bufferToBase64(resultBuffer));
+      } catch (error: any) {
+        reject(error.message);
+      }
+    });
+  }
 
-//   /**
-//    * decrypt data, return string
-//    * @param password - password must be required for decrypt
-//    * @param data - data for decrypt
-//    */
+  /**
+   * decrypt data, return string
+   * @param password - password must be required for decrypt
+   * @param data - data for decrypt
+   */
 
-//   decrypt(password: string, data: string): Promise<string> {
-//     if (password === '' && data === '') {
-//       throw new Error('password and data is required');
-//     }
-//     if (typeof password !== 'string' && typeof data !== 'string') {
-//       throw new Error('password and data must be string');
-//     }
+  decrypt(password: string, data: string): Promise<string> {
+    if (password === '' && data === '') {
+      throw new Error('password and data is required');
+    }
+    if (typeof password !== 'string' && typeof data !== 'string') {
+      throw new Error('password and data must be string');
+    }
 
-//     return new Promise(async (resolve, reject) => {
-//       try {
-//         const passkey = await this.passkey(password);
-//         const aesKey = await this.aesKey(passkey);
-//         const dataBuffer = this.base64ToBuffer(data);
-//         const resultBuffer = await this.aesDecrypt(aesKey, dataBuffer);
-//         resolve(this.bufferToString(resultBuffer));
-//       } catch (error) {
-//         reject(error);
-//       }
-//     });
-//   }
-// }
+    return new Promise(async (resolve, reject) => {
+      try {
+        const passkey = await this.instanceHelper.passkey(password);
+        const aesKey = await this.instanceHelper.aesKey(passkey);
+        const dataBuffer = this.instanceCommon.base64ToBuffer(data);
+        const resultBuffer = await this.instanceHelper.aesDecrypt(
+          aesKey,
+          dataBuffer
+        );
+        resolve(this.instanceCommon.bufferToString(resultBuffer));
+      } catch (error: any) {
+        reject(error.message);
+      }
+    });
+  }
+}
 
-// export default JSPBKDF2FE;
+export default JSPBKDF2FE;
